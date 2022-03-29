3a302ed2dd035366d0148ab5026b0ed70794d2cc
miintTeaa <rebloxminer@gmail.com>
2022-03-29 21:56:17 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Couldn't read file");
+
+    println!("With text:\n{}", contents);
+}
