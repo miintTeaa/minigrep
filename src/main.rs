037642379840f05ef7a875485413fafa9dbd7f3a
miintTeaa <rebloxminer@gmail.com>
2022-03-29 23:58:21 +0200

    Extracted run function, further upgraded error handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let config: Config = match Config::new(&args) {
         Ok(config) => config,
         Err(e) => panic!("Failed to parse config: {}", e),
     };
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    run(&config)
+}
 
-    let contents = fs::read_to_string(config.filename).expect("Couldn't read file");
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    println!("Searching for {}", &config.query);
+    println!("In file {}", &config.filename);
+
+    //Use of ? operator: if read to string returns an error,
+    //will stop executing this function and return that error.
+    let contents: String = fs::read_to_string(&config.filename)?;
 
     println!("With text:\n{}", contents);
+    Ok(())
 }
 
 struct Config {
