047df8aae318d702184247de6a14201502549736
miintTeaa <rebloxminer@gmail.com>
2022-03-29 23:06:34 +0200

    Updated config parsing

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,30 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config: Config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Couldn't read file");
+    let contents = fs::read_to_string(config.filename).expect("Couldn't read file");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        Config {
+            query: args[1].clone(),
+            filename: args[2].clone(),
+        }
+    }
+}
+
+fn parse_config(args: &[String]) -> Config {
+    Config::new(args)
 }
