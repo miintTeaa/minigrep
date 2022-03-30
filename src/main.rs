65cd81956566b52b1d6275cb36315da7d3de82cd
miintTeaa <rebloxminer@gmail.com>
2022-03-30 03:21:39 +0200

    Added help

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,23 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    if ["--help", "--h", "-help", "-h", "help", "h"].contains(&args[1].as_str()) {
+        println!(
+            "\
+Usage: minigrep <query> <filename> args
+Possible arguments:
+--case: Does search using case sensitive mode
+--nocase: Does search using case insensitive mode
+
+You can also set case sensitivity mode using an environment variable:
+CASE_INSENSITIVE will cause every search to be case insensitive by default if set to \"yes\" (unless overriden by an argument)
+"
+        );
+        
+        process::exit(0);
+    }
+
     let config: Config = match Config::new(&args) {
         Ok(config) => config,
         Err(e) => {
